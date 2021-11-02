d934f33a8206646a0aacac42013b81542737da0b
Nicholas Velten <nikvelten@gmail.com>
2021-11-02 15:10:50 -0300

    TypeScript API

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,32 @@
+import User from "../entities/User";
+import { EntityRepository, Repository } from "typeorm";
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        name,
+      },
+    });
+    return user;
+  }
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        id,
+      },
+    });
+    return user;
+  }
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
+}
+
+export default UsersRepository;
